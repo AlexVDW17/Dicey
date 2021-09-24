54a005a366911b497aaced76d790b74c4d50b97b
Alex Van De Wiele <avandewi@uwaterloo.ca>
2021-09-24 10:08:55 -0400

    bot now working for basic rolls of form roll xDy+z

--- rolldice.py
+++ rolldice.py
@@ -21,24 +21,46 @@ def multiDiceMod(dice):
     dice = re.split('\+',dice)
     #call the multiDice function
     result = multiDice(dice[0])
-    #add the modifier
-    result += int(dice[1])
+    #check if there is a modifier
+    if len(dice) > 1:
+        result += int(dice[1])
     return result
 
 #so we have the dice functionality
 #now we need to communicate with discord and get the user input
 import os
 import discord
-
+import random
 from dotenv import load_dotenv
+from discord.ext import commands
 load_dotenv()
 #get the token from the .env file
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
-
-@client.event
+#client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
 
-client.run(TOKEN)
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+
+# a bot command that takes in a string of xDy+z where x is the number of dice, y is the number of sides, and z is the modifier
+# then uses the multiDiceMod function to return a result
+@bot.command(name='roll', help='rolls a dice')
+async def roll(ctx, *args):
+    #get the input
+    input = ''.join(args)
+    #call the multiDiceMod function
+    result = multiDiceMod(input)
+    #send the result
+    await ctx.send(result)
+
+bot.run(TOKEN)
